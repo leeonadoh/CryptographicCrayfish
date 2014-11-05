9ee1cb8c504e7459c6301e58c0a7d9865c287b0d
Sunpeng Li <lisun@ic406-152.(none)>
2014-11-05 18:20:40 -0500

    Modified for python3

--- A2/rsaHax/brent.py
+++ A2/rsaHax/brent.py
@@ -2,15 +2,22 @@ from random import randint
 
 y,c,m = 1,1,1
 g,r,q = 1,1,1
-k = 0
+k = 0   
 x = 0
 ys = 0
 
-def gcd(a,b):
+gcdA = 0;
+gcdB = 0;
+gcdResult = 0;
+
+def gcd():
     """ the euclidean algorithm """
-    while a:
-        a, b = b%a, a
-    return b
+    global gcdA
+    global gcdB
+    global gcdResult
+    while gcdA:
+        gcdA, gcdB = gcdB%gcdA, gcdA
+    gcdResult = gcdB
 
 def brent(N):
     global y
@@ -22,6 +29,10 @@ def brent(N):
     global k
     global x
     global ys
+    global gc
+    global gcdA
+    global gcdB
+    global gcdResult
     m = randint(1, N-1)
     c = randint(1, N-1)
     y = randint(1, N-1)
@@ -30,20 +41,26 @@ def brent(N):
     while g==1:            
         x = y
         for i in range(r):
-                y = ((y*y)%N    +c)%N
+                y = ((y*y)%N +c)%N
         k = 0
         while (k<r and g==1):
             ys = y
             for i in range(min(m,r-k)):
                     y = ((y*y)%N+c)%N
                     q = q*(abs(x-y))%N
-            g = gcd(q,N)
+            gcdA = q
+            gcdB = N
+            gcd()
+            g = gcdResult
             k = k + m
         r = r*2
     if g==N:
         while True:
             ys = ((ys*ys)%N+c)%N
-            g = gcd(abs(x-ys),N)
+            gcdA = abs(x-ys)
+            gcdB = N
+            gcd()
+            g = gcdResult
             if g>1:
                 break
  
@@ -51,4 +68,4 @@ def brent(N):
 
 if __name__ == '__main__':
     factor = brent(127364267597139493540723331204339211194586014817451203830799795925196194691202462897905850883866904868892415046580817569176239367692303288839770474652109700848358432405752683726342528889678012214522325274056903064820951043366005591893083764579470069805619180603771671383915933692672583275832310594117217293261)
-    print factor;
+    print(factor);
